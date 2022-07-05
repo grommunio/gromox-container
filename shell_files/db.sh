513948eab5329b5ee67ca0a8ebcf581bc73d82e4
Olamide Omolola <olamide.omolola@grommunio.com>
2022-07-05 18:58:20 +0200

    added db shell script

--- shell_files/db.sh
+++ shell_files/db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-create_mysql_adaptor_conf(){
+DB_HOST='db'
+
+generate_mysql_adaptor_conf(){
   FILENAME=$1 
   
   declare -A INPUTS=(
@@ -9,7 +11,7 @@ create_mysql_adaptor_conf(){
    [mysql_dbname]=$MARIADB_DATABASE
   )
   
-  touch ${FILENAME}
+  : > ${FILENAME}
 
   for elem in "${!INPUTS[@]}"
   do
@@ -17,4 +19,48 @@ create_mysql_adaptor_conf(){
   done
 }
 
-create_mysql_adaptor_conf "/etc/gromox/mysql_adaptor.cfg"
+generate_g_cf_files(){
+  FILENAME=$1 
+
+  declare -A INPUTS=(
+   [user]=$MARIADB_USER
+   [password]=$MARIADB_PASSWORD
+   [dbname]=$MARIADB_DATABASE
+   [hosts]=$DB_HOST
+   [query]=$2
+  )
+  
+  : > ${FILENAME}
+
+  for elem in "${!INPUTS[@]}"
+  do
+   echo "${elem} = ${INPUTS[${elem}]}" >> ${FILENAME}
+  done
+}
+
+generate_admin_db_conf(){
+  FILENAME=$1 
+
+  declare -A INPUTS=(
+   [user]=$MARIADB_USER
+   [pass]=$MARIADB_PASSWORD
+   [database]=$MARIADB_DATABASE
+   [host]=$DB_HOST
+  )
+  
+  echo "DB:" > ${FILENAME}
+
+  for elem in "${!INPUTS[@]}"
+  do
+   echo "  ${elem}: '${INPUTS[${elem}]}'" >> ${FILENAME}
+  done
+}
+#generate_mysql_adaptor_conf "/etc/gromox/mysql_adaptor.cfg"
+#generate_g_cf_files "/etc/gromox/g-alias.cf" "SELECT mainname FROM aliases WHERE aliasname='%s'"
+#generate_g_cf_files "/etc/gromox/g-virt.cf" "SELECT 1 FROM domains WHERE domain_status=0 AND domainname='%s'"
+#generate_admin_db_conf "/etc/grommunio-admin-api/conf.d/database.yaml"
+
+#generate_mysql_adaptor_conf "mysql_adaptor.cfg"
+#generate_admin_db_conf "database.yaml"
+#generate_g_cf_files "g-alias.cf" "SELECT mainname FROM aliases WHERE aliasname='%s'"
+#generate_g_cf_files "g-virt.cf" "SELECT 1 FROM domains WHERE domain_status=0 AND domainname='%s'"
