2b878c841ce74d769589b52524d63421fbad9455
Olamide Omolola <olamide.omolola@grommunio.com>
2022-10-26 17:29:12 +0200

    added certmanager for cert management

--- k8s_files/set-up.sh
+++ k8s_files/set-up.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-echo 'This script assumes that you have a working kubernetes cluster and the kubectl context is set to it'
+echo 'This script assumes that you have a working kubernetes cluster, the kubectl context is set to it and helm v3 installed (all latest versions)' 
+
+# Add helm repositories
+helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+# Setup Grommunio Jitsi
+helm install \
+	gromox-meet jitsi/jitsi-meet \
+	-f video/video-values.yml
+
+# Setup cert-manager
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.10.0 \
+  --set installCRDs=true \
+  --create-namespace
+
+helm repo add jetstack https://charts.jetstack.io
+sleep 30s
 
 kubectl apply -f db-config.yaml
 kubectl apply -f ssl-config.yaml
@@ -15,5 +36,3 @@ kubectl apply -f init-db.yaml
 sleep 90s
 kubectl apply -f gromox.yaml
 sleep 60s
-helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
-helm install gromox-meet jitsi/jitsi-meet -f video/video-values.yml
