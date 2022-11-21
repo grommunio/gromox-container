487a02cfad63a0b2441e15a044a87574b035f5df
Olamide Omolola <olamide.omolola@grommunio.com>
2022-11-21 16:42:19 +0100

    update

--- build-assets/services/config-setup/config-setup.sh
+++ build-assets/services/config-setup/config-setup.sh
@@ -5,7 +5,15 @@ if [ -f /etc/php7/fpm/php-fpm.conf.default ]; then
 fi
 
 ln -s /home/gromox-services/* /etc/gromox/
-ln -s /home/nginx/ss_certificate.conf /etc/grommunio-common/nginx/ssl_certificate.conf
-ln -s /etc/grommunio-common/nginx/ssl_certificate.conf /etc/grommunio-admin-common/nginx-ssl.conf
+
+if [ -f /etc/grommunio-common/nginx/ssl_certificate.conf ]; then
+	ln -s /home/nginx/ss_certificate.conf /etc/grommunio-common/nginx/ssl_certificate.conf
+fi
+
+
+if [ -f /etc/grommunio-admin-common/nginx-ssl.conf]; then
+	ln -s /etc/grommunio-common/nginx/ssl_certificate.conf /etc/grommunio-admin-common/nginx-ssl.conf
+fi
+
 chown root:gromox /etc/gromox  
 chmod 775 /etc/gromox 
