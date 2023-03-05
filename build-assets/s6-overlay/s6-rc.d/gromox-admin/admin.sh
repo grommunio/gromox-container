204c9ca0176e45428b035d7bfa1116fdb3822522
Fabian Stanke <me+github@fmos.at>
2023-03-05 01:11:45 -0500

    Cleaned service script structure

--- build-assets/s6-overlay/s6-rc.d/gromox-admin/admin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv sh
-
-sed -i 's/chmod-socket = 660/chmod-socket = 666/g' /usr/share/grommunio-admin-api/api-config.ini
-
-uwsgi --ini /usr/share/grommunio-admin-api/api-config.ini 
--- build-assets/s6-overlay/s6-rc.d/gromox-postfix/gromox-postfix.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh -e
-
-
-postfix start
