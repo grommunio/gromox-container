375206a2bdefe0cd9e12a8b13686cac2f1b2bf4f
Olamide Omolola <olamide.omolola@grommunio.com>
2023-05-05 15:33:43 +0200

    updated dependencies

--- grommunio-web/build-assets/s6-overlay/s6-rc.d/db-setup/db.sh
+++ grommunio-web/build-assets/s6-overlay/s6-rc.d/db-setup/db.sh
@@ -55,21 +55,21 @@ generate_admin_db_conf(){
   done
 }
 
-generate_mysql_adaptor_conf "/etc/gromox/mysql_adaptor.cfg"
-generate_admin_db_conf "/etc/grommunio-admin-api/conf.d/database.yaml"
-generate_g_cf_files "/etc/postfix/g-alias.cf" "SELECT mainname FROM aliases WHERE aliasname='%s'"
-generate_g_cf_files "/etc/postfix/g-virt.cf" "SELECT 1 FROM domains WHERE domain_status=0 AND domainname='%s'"
+#generate_mysql_adaptor_conf "/etc/gromox/mysql_adaptor.cfg"
+#generate_admin_db_conf "/etc/grommunio-admin-api/conf.d/database.yaml"
+#generate_g_cf_files "/etc/postfix/g-alias.cf" "SELECT mainname FROM aliases WHERE aliasname='%s'"
+#generate_g_cf_files "/etc/postfix/g-virt.cf" "SELECT 1 FROM domains WHERE domain_status=0 AND domainname='%s'"
 
 
 # Set up DB only when necessary
 
 
-HAS_TABLES=$(mysql -u ${MARIADB_USER} -h ${DB_HOST} -p"${MARIADB_PASSWORD}" -D ${MARIADB_DATABASE} --execute="SELECT CASE COUNT(*) WHEN '0' THEN 'false' ELSE 'true' END AS contents FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = '${MARIADB_DATABASE}';")
+#HAS_TABLES=$(mysql -u ${MARIADB_USER} -h ${DB_HOST} -p"${MARIADB_PASSWORD}" -D ${MARIADB_DATABASE} --execute="SELECT CASE COUNT(*) WHEN '0' THEN 'false' ELSE 'true' END AS contents FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = '${MARIADB_DATABASE}';")
 
 
-if [[ $HAS_TABLES =~ "false" ]]; then
-	echo 'DB is not populated, populating it...'
-	gromox-dbop -C
-else
-	echo 'DB is popoulated. Skipping'
-fi
+#if [[ $HAS_TABLES =~ "false" ]]; then
+#	echo 'DB is not populated, populating it...'
+#	gromox-dbop -C
+#else
+#	echo 'DB is popoulated. Skipping'
+#fi
