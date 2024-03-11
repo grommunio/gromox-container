f535363a2104252f0ef5b6f06a9fb9aa04d870c7
Olamide Omolola <olamide.omolola@grommunio.com>
2024-03-11 11:47:27 +0100

    added firewall script to gromox core

--- gromox-core/entrypoint.sh
+++ gromox-core/entrypoint.sh
@@ -263,22 +263,10 @@ postconf -P submission/inet/milter_macro_daemon_name=ORIGINATING
 systemctl enable postfix.service >>"${LOGFILE}" 2>&1
 systemctl restart postfix.service >>"${LOGFILE}" 2>&1
 
-systemctl enable grommunio-fetchmail.timer >>"${LOGFILE}" 2>&1
-systemctl start grommunio-fetchmail.timer >>"${LOGFILE}" 2>&1
+systemctl enable firewalld.service grommunio-fetchmail.timer >>"${LOGFILE}" 2>&1
+systemctl start firewalld.service grommunio-fetchmail.timer >>"${LOGFILE}" 2>&1
 
-{
-  firewall-cmd --add-service=https --zone=public --permanent
-  firewall-cmd --add-port=25/tcp --zone=public --permanent
-  firewall-cmd --add-port=80/tcp --zone=public --permanent
-  firewall-cmd --add-port=110/tcp --zone=public --permanent
-  firewall-cmd --add-port=143/tcp --zone=public --permanent
-  firewall-cmd --add-port=587/tcp --zone=public --permanent
-  firewall-cmd --add-port=993/tcp --zone=public --permanent
-  firewall-cmd --add-port=995/tcp --zone=public --permanent
-  firewall-cmd --add-port=8080/tcp --zone=public --permanent
-  firewall-cmd --add-port=8443/tcp --zone=public --permanent
-  firewall-cmd --reload
-} >>"${LOGFILE}" 2>&1
+. "/home/scripts/firewall.sh"
 
 systemctl restart redis@grommunio.service nginx.service php-fpm.service gromox-delivery.service \
   gromox-event.service gromox-http.service gromox-imap.service gromox-midb.service \
